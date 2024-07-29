6c03c4b1ff5f4e4499adbd1febcebd5b9e5a7c49
kvernert <kverner47@gmail.com>
2024-07-29 11:58:55 -0300

    Adiciona relacionamento de Livro com Categoria e Editora

--- core/models/autor.py
+++ core/models/autor.py
@@ -3,6 +3,8 @@ from django.db import models
 class Autor(models.Model):
     nome = models.CharField(max_length=100)
     email = models.CharField(max_length=100, blank=True, null=True)
-
+    class Meta:
+        verbose_name = "Autor"
+        verbose_name_plural = "Autores"
     def __str__(self):
         return self.nome
