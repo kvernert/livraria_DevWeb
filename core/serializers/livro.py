5b4322f974ca6d8e28143c124236973057b0a72c
kvernert <kverner47@gmail.com>
2024-08-16 14:52:22 -0300

    Dois serializadores para Livro

---
+++ core/serializers/livro.py
@@ -0,0 +1,22 @@
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
+
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
+
+from core.models import Livro 
+
+class LivroSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+
+class LivroDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1        
+
+class LivroListSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = ["id", "titulo", "preco"]
